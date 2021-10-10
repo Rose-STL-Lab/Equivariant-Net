f86a8c8f8371bce4a27bc74e0fb41d683d6dd244
Ray <32885937+Rui1521@users.noreply.github.com>
2021-10-10 16:56:40 -0700

    Add files via upload

---
+++ evaluation.py
@@ -0,0 +1,85 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from torch.utils import data
+from torch.autograd import Variable
+import os
+
+def azimuthalAverage(image, center=None):
+    """
+    Calculate the azimuthally averaged radial profile.
+
+    image - The 2D image
+    center - The [x,y] pixel coordinates used as the center. The default is 
+             None, which then uses the center of the image (including 
+             fracitonal pixels).
+    
+    """
+    # Calculate the indices from the image
+    y, x = np.indices(image.shape)
+
+    if not center:
+        center = np.array([(x.max()-x.min())/2.0, (x.max()-x.min())/2.0])
+
+    r = np.hypot(x - center[0], y - center[1])
+
+    # Get sorted radii
+    ind = np.argsort(r.flat)
+    r_sorted = r.flat[ind]
+    i_sorted = image.flat[ind]
+
+    # Get the integer part of the radii (bin size = 1)
+    r_int = r_sorted.astype(int)
+
+    # Find all pixels that fall within each radial bin.
+    deltar = r_int[1:] - r_int[:-1]  # Assumes all radii represented
+    rind = np.where(deltar)[0]       # location of changed radius
+    nr = rind[1:] - rind[:-1]        # number of radius bin
+    
+    # Cumulative sum to figure out sums for each radius bin
+    csim = np.cumsum(i_sorted, dtype=float)
+    tbin = csim[rind[1:]] - csim[rind[:-1]]
+
+    radial_prof = tbin / nr
+
+    return radial_prof
+
+
+def TKE(preds):
+    """
+    Calculate the TKE field of the predictions
+    """
+    mean_flow = np.expand_dims(np.mean(preds, axis = 0), axis = 0)
+    tur_preds = np.mean((preds - mean_flow)**2, axis = 0)
+    tke = (tur_preds[0] + tur_preds[1])/2
+    return tke
+
+def tke2spectrum(tke):
+    """
+    Convert TKE field to spectrum
+    """
+    sp = np.fft.fft2(tke)
+    sp = np.fft.fftshift(sp)
+    sp = np.real(sp*np.conjugate(sp))
+    sp1D = radialProfile.azimuthalAverage(sp)
+    return np.log10(sp1D)
+ 
+def spectrum_band(tensor):
+    """
+    Calculate spectrum_band of predictions
+    """
+    spec = np.array([tke2spectrum(TKE(tensor[i])) for i in range(tensor.shape[0])])
+    return np.mean(spec, axis = 0)
+
+
+
+spec_rmse = []
+rmse = []
+for i in range(1, 6):
+    direc = ".../ResNet_"
+    file = torch.load(direc + str(i) +"_time.pt")
+    preds = file["preds"]
+    trues = file["trues"]
+    spec_rmse.append(np.sqrt(np.mean((spectrum_band(preds) - spectrum_band(trues))**2)))
+    rmse.append(file["rmse"])
+print(np.mean(rmse), np.mean(spec_rmse))
